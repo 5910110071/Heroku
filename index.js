0bdd8e1c22c378f7b61e2553bc9d67feb775adbb
emailpsu\5910110071 <cnp@ii.co.th>
2020-11-18 11:03:54 +0700

    commit 2

--- index.js
+++ index.js
@@ -47,36 +47,36 @@ const {
     agent.handleRequest(intentMap);
   });
 
-app.post('/products', async (req, res) => {
-    const payload = req.body
-    const product = new Product(payload)
-    await product.save()
-    res.status(201).end()
-})
+// app.post('/products', async (req, res) => {
+//     const payload = req.body
+//     const product = new Product(payload)
+//     await product.save()
+//     res.status(201).end()
+// })
 
-app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-})
+// app.get('/products', async (req, res) => {
+//     const products = await Product.find({})
+//     res.json(products)
+// })
 
-app.get('/products/:id', async (req, res) => {
-    const { id } = req.params
-    const product = await Product.findById(id)
-    res.json(product)
-})
+// app.get('/products/:id', async (req, res) => {
+//     const { id } = req.params
+//     const product = await Product.findById(id)
+//     res.json(product)
+// })
 
-app.put('/products/:id', async (req, res) => {
-    const payload = req.body
-    const { id } = req.params
-    const product = await Product.findByIdAndUpdate(id, { $set: payload })
-    res.json(product)
-})
+// app.put('/products/:id', async (req, res) => {
+//     const payload = req.body
+//     const { id } = req.params
+//     const product = await Product.findByIdAndUpdate(id, { $set: payload })
+//     res.json(product)
+// })
 
-app.delete('/products/:id', async (req, res) => {
-    const { id } = req.params
-    await Product.findByIdAndDelete(id)
-    res.status(204).end()
-})
+// app.delete('/products/:id', async (req, res) => {
+//     const { id } = req.params
+//     await Product.findByIdAndDelete(id)
+//     res.status(204).end()
+// })
 
 app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
